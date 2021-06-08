3de6bbfe9340e7227a821c8279b6477a86733d15
kevincastro015 <kevin.castro015@mymdc.net>
2021-06-08 00:42:17 +0000

    firststage

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function randomText() {
+    document.querySelector("generateExcuse").addEventListener("click",() => {
+    document.querySelector("excuse").innerHTML = generateExcuse();
+}
+
+let generateExcuse = () => {
+    let who = ['The dog','My grandma','His turtle','My bird'];
+    let action = ['ate','peed','crushed','broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class','right on time','when I finished','during my lunch','while I was praying']
+
+    let whoIndx = Math.floor(Math.random() * who.length);
+    let actionIndx = Math.floor(Math.random() * action.length);
+    let whatIndx = Math.floor(Math.random() * what.length);
+    let whenIndx = Math.floor(Math.random() * when.length);
+
+    return who[whoIndx] + '' + action[actionIndx] + '' + what[whatIndx] + '' + when[whenIndx];
+}
+let generateExcuse = randomText()
+console.log(randomExcuse)
